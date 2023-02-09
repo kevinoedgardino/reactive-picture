5687f4c8fdd1a6244836bd0d9a6d8cdd1d53d75f
Kevin Chacon <kevinchacon3211@gmail.com>
2023-02-09 13:09:21 -0600

    Create file to export dom elements

---
+++ src/js/dom-elements.js
@@ -0,0 +1,4 @@
+export const IMAGE_FILE_ELEMENT = document.getElementById('file-image')
+export const AUDIO_FILE_ELEMENT = document.getElementById('file-audio')
+export const IMAGE_ELEMENT = document.getElementById('picture')
+export const AUDIO_ELEMENT = document.getElementById('audio')
