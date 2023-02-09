175097a58c51b7e03a39deca17b200e1b5d138ca
Kevin Chacon <kevinchacon3211@gmail.com>
2023-02-09 13:09:05 -0600

    Create file for freq handler code

---
+++ src/js/audio-freq-handler.js
@@ -0,0 +1,27 @@
+import { AUDIO_ELEMENT, AUDIO_FILE_ELEMENT } from './dom-elements'
+
+const FREQUENCY_BIN_COUNT = 32
+const dataArray = new Uint8Array(FREQUENCY_BIN_COUNT)
+
+AUDIO_FILE_ELEMENT.onchange = () => {
+  // eslint-disable-next-line no-undef
+  const reader = new FileReader()
+  reader.onload = (e) => { AUDIO_ELEMENT.src = e.target.result }
+  reader.readAsDataURL(AUDIO_FILE_ELEMENT.files[0])
+}
+
+let audioCtx = null
+let analyser = null
+
+export function setAudioCtx () {
+  audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+  analyser = audioCtx.createAnalyser()
+  const source = audioCtx.createMediaElementSource(AUDIO_ELEMENT)
+  source.connect(analyser)
+  analyser.connect(audioCtx.destination)
+}
+
+export function useFrequency (handlerFunction) {
+  analyser.getByteFrequencyData(dataArray)
+  handlerFunction(dataArray)
+}
